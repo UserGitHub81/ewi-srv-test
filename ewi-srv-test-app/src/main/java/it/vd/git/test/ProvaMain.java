e69d06cec6c798522caf0449c6f16f8dad5a97cb
Master-PC\Master <u1484032@mvrht.net>
2018-02-17 18:49:59 +0100

    inserimento file progetto

---
+++ ewi-srv-test-app/src/main/java/it/vd/git/test/ProvaMain.java
@@ -0,0 +1,11 @@
+package it.vd.git.test;
+
+public class ProvaMain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello word");
+		
+	}
+
+}
